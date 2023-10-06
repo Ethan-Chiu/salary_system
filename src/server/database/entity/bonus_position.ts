12ad32e04788158594575b1333bb61dbc6ce4dae
Ethan-Chiu <ethanchiu0608@gmail.com>
2023-10-06 14:34:55 +0800

    alter bonus position

--- src/server/database/entity/bonus_position.ts
+++ src/server/database/entity/bonus_position.ts
@@ -1,13 +1,16 @@
 import { Entity, Column, PrimaryGeneratedColumn } from "typeorm"
 import { BaseMeta } from "./base_meta"
 
-@Entity("U_BONUS_RANK")
-export class BonusRank extends BaseMeta {
+@Entity("U_BONUS_POSITION")
+export class BonusPosition extends BaseMeta {
     @PrimaryGeneratedColumn()
     id: number
 
     @Column("int")
-    rank: number
+    position: number
+
+    @Column("nvarchar2", {length: 2})
+    position_type: string
 
     @Column("float")
     multiplier: number
