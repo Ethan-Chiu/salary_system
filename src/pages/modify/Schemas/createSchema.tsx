6dc6aeaa3f2b144dbcadd912ebfb1ce761feecd7
ponywang0506 <b09901060@ntu.edu.tw>
2023-12-23 23:46:16 +0800

    rewrite schema in modify page

--- src/pages/modify/Schemas/createSchema.tsx
+++ src/pages/modify/Schemas/createSchema.tsx
@@ -1,4 +1,6 @@
+import { randomInt } from "crypto";
 import * as z from "zod";
+import { isNumber, isDate, isString } from "~/pages/develop_parameters/utils/checkType";
 import { Translate } from "~/pages/develop_parameters/utils/translation";
 
 function getTranslate(key: string) {
@@ -6,16 +8,16 @@ function getTranslate(key: string) {
 }
 
 function getRequiredError(key: string) {
-	return {required_error: getTranslate(key) + " is required."}
+	return {required_error: getTranslate(key) + " is required."};
 }
 
 // input: [{key = key1, type = "string" | "number" | "date" required = true}, {}, {}]
-export function createOneKeySchema(data: any) {
-	let schema;
+function createOneKeySchema(data: any, defaultValue?: any) {
+	let schema = undefined;
 	if(data.required === false) {
-		if(data.type === "string") schema = z.coerce.string();
-		if(data.type === "number") schema = z.coerce.number();
-		if(data.type === "date") schema = z.coerce.date();
+		if(data.type === "string") schema = z.coerce.string().optional();
+		if(data.type === "number") schema = z.coerce.number().optional();
+		if(data.type === "date") schema = z.coerce.date().optional();
 	}
 	else {
 		if(data.type === "string") 
@@ -23,38 +25,36 @@ export function createOneKeySchema(data: any) {
 		if(data.type === "number") schema = z.coerce.number(getRequiredError(data.key));
 		if(data.type === "date") schema = z.coerce.date(getRequiredError(data.key));
 	}
-	schema = schema!.describe(getTranslate(data.key));
+
+	schema = (schema as any).describe(getTranslate(data.key));
+	
+	if(defaultValue || defaultValue === 0) {
+		if(data.type === "date")
+			schema = (schema as any).default(new Date(defaultValue));
+		else if(data.type === "number")	{
+			console.log(data);
+			console.log(defaultValue);
+			schema = (schema as any).default(defaultValue);
+		}
+		else if(data.type === "string")
+			schema = (schema as any).default(defaultValue);
+	}
 	return schema;
 }
 
-
-
-
-// const x = [
-// 	{ key: "personal_leave_dock", type: "number" },
-// 	{ key: "sick_leave_dock", type: "number" },
-// 	{ key: "rate_of_unpaid_leave", type: "number" },
-// 	{ key: "unpaid_leave_compensatory_1", type: "number" },
-// 	{ key: "unpaid_leave_compensatory_2", type: "number" },
-// 	{ key: "unpaid_leave_compensatory_3", type: "number" },
-// 	{ key: "unpaid_leave_compensatory_4", type: "number" },
-// 	{ key: "unpaid_leave_compensatory_5", type: "number" },
-// 	{ key: "overtime_by_foreign_workers_1", type: "number" },
-// 	{ key: "overtime_by_foreign_workers_2", type: "number" },
-// 	{ key: "overtime_by_foreign_workers_3", type: "number" },
-// 	{ key: "overtime_by_local_workers_1", type: "number" },
-// 	{ key: "overtime_by_local_workers_2", type: "number" },
-// 	{ key: "overtime_by_local_workers_3", type: "number" },
-// 	{ key: "local_worker_holiday", type: "number" },
-// 	{ key: "foreign_worker_holiday", type: "number" },
-// 	{ key: "start date", type: "number" },
-//   ];
+export function createSchema(config: any, defaultValues?: any) {
+	let schemas: any = {};
   
+	for (const c of config) {
+	  const schema = (c.key in defaultValues)
+		? createOneKeySchema(c, defaultValues[c.key])
+		: createOneKeySchema(c);
+  
+	  schemas[c.key] = schema;
+	}
+	
+	console.log(schemas);
 
-// export const attendanceSchema = (data: any) => {
-// 	const allSchema = x.reduce((acc: any, curr: any) => {
-// 		acc[curr.key] = createOneKeySchema(curr);
-//   		return acc;
-// 	}, {})
-
-// }
+	return z.object(schemas);
+  }
+  
