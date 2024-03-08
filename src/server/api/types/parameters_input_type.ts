791a0b7f1d96a556d96e5febe34f5bead9237a6e
Max <max2030168@gmail.com>
2024-03-08 10:32:06 +0800

    create employee trust service

--- src/server/api/types/parameters_input_type.ts
+++ src/server/api/types/parameters_input_type.ts
@@ -248,6 +248,19 @@ export const createEmployeePaymentService = EmployeePayment.merge(DateService);
 export const updateEmployeePaymentAPI = EmployeePayment.merge(Id).merge(DateAPI).partial();
 export const updateEmployeePaymentService = EmployeePayment.merge(Id).merge(DateService).partial();
 
+const EmployeeTrust = z.object({
+	emp_no: z.string(),
+	emp_trust_reserve: z.number(),
+	org_trust_reserve: z.number(),
+	emp_special_trust_incent: z.number(),
+	org_special_trust_incent: z.number(),
+});
+
+export const createEmployeeTrustAPI = EmployeeTrust.merge(DateAPI);
+export const createEmployeeTrustService = EmployeeTrust.merge(DateService);
+export const updateEmployeeTrustAPI = EmployeeTrust.merge(Id).merge(DateAPI).partial();
+export const updateEmployeeTrustService = EmployeeTrust.merge(Id).merge(DateService).partial();
+
 const EmployeeData = z.object({
 	emp_no: z.string(),
 	emp_name: z.string(),
---
+++ src/server/service/employee_trust_service.ts
@@ -0,0 +1,146 @@
+import { injectable } from "tsyringe";
+import { BaseResponseError } from "../api/error/BaseResponseError";
+import { check_date, get_date_string, select_value } from "./helper_function";
+import { type z } from "zod";
+import {
+    type createEmployeeTrustService,
+    type updateEmployeeTrustService,
+} from "../api/types/parameters_input_type";
+import { Op } from "sequelize";
+import { EmployeeTrust } from "../database/entity/SALARY/employee_trust";
+
+@injectable()
+export class EmployeeTrustService {
+    /* constructor() {} */
+
+    async createEmployeeTrust({
+        emp_no,
+        emp_trust_reserve,
+        org_trust_reserve,
+        emp_special_trust_incent,
+        org_special_trust_incent,
+        start_date,
+        end_date,
+    }: z.infer<typeof createEmployeeTrustService>): Promise<EmployeeTrust> {
+        const current_date_string = get_date_string(new Date());
+        check_date(start_date, end_date, current_date_string);
+        const newData = await EmployeeTrust.create({
+            emp_no: emp_no,
+            emp_trust_reserve: emp_trust_reserve,
+            org_trust_reserve: org_trust_reserve,
+            emp_special_trust_incent: emp_special_trust_incent,
+            org_special_trust_incent: org_special_trust_incent,
+            start_date: start_date ?? current_date_string,
+            end_date: end_date,
+            create_by: "system",
+            update_by: "system",
+        });
+        return newData;
+    }
+
+    async getEmployeeTrustById(id: number): Promise<EmployeeTrust | null> {
+        const employeeTrust = await EmployeeTrust.findOne({
+            where: {
+                id: id,
+            },
+        });
+        return employeeTrust;
+    }
+
+    async getCurrentEmployeeTrust(): Promise<EmployeeTrust[]> {
+        const current_date_string = get_date_string(new Date());
+        const employeeTrust = await EmployeeTrust.findAll({
+            where: {
+                start_date: {
+                    [Op.lte]: current_date_string,
+                },
+                end_date: {
+                    [Op.or]: [
+                        { [Op.gte]: current_date_string },
+                        { [Op.eq]: null },
+                    ],
+                },
+            },
+        });
+        return employeeTrust;
+    }
+
+    async getCurrentEmployeeTrustById(
+        id: number
+    ): Promise<EmployeeTrust | null> {
+        const current_date_string = get_date_string(new Date());
+        const employeeTrust = await EmployeeTrust.findOne({
+            where: {
+                id: id,
+                start_date: {
+                    [Op.lte]: current_date_string,
+                },
+                end_date: {
+                    [Op.or]: [
+                        { [Op.gte]: current_date_string },
+                        { [Op.eq]: null },
+                    ],
+                },
+            },
+        });
+        return employeeTrust;
+    }
+
+    async getAllEmployeeTrust(): Promise<EmployeeTrust[]> {
+        const employeeTrust = await EmployeeTrust.findAll();
+        return employeeTrust;
+    }
+
+    async updateEmployeeTrust({
+        id,
+        emp_no,
+        emp_trust_reserve,
+        org_trust_reserve,
+        emp_special_trust_incent,
+        org_special_trust_incent,
+        start_date,
+        end_date,
+    }: z.infer<typeof updateEmployeeTrustService>): Promise<void> {
+        const employeeTrust = await this.getEmployeeTrustById(id!);
+        if (employeeTrust == null) {
+            throw new BaseResponseError("Employee Trust does not exist");
+        }
+        const affectedCount = await EmployeeTrust.update(
+            {
+                emp_no: select_value(emp_no, employeeTrust.emp_no),
+                emp_trust_reserve: select_value(
+                    emp_trust_reserve,
+                    employeeTrust.emp_trust_reserve
+                ),
+                org_trust_reserve: select_value(
+                    org_trust_reserve,
+                    employeeTrust.org_trust_reserve
+                ),
+                emp_special_trust_incent: select_value(
+                    emp_special_trust_incent,
+                    employeeTrust.emp_special_trust_incent
+                ),
+                org_special_trust_incent: select_value(org_special_trust_incent, employeeTrust.org_special_trust_incent),
+                start_date: select_value(
+                    start_date,
+                    employeeTrust.start_date
+                ),
+                end_date: select_value(end_date, employeeTrust.end_date),
+                update_by: "system",
+            },
+            { where: { id: id } }
+        );
+        if (affectedCount[0] != 1) {
+            throw new BaseResponseError("Update error");
+        }
+    }
+
+    async deleteEmployeeTrust(id: number): Promise<void> {
+        const destroyedRows = await EmployeeTrust.destroy({
+            where: { id: id },
+        });
+        if (destroyedRows != 1) {
+            throw new BaseResponseError("Delete error");
+        }
+    }
+}
