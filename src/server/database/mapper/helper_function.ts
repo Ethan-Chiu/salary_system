ad6a09ddd82c99ab31219ff42b136ba668887567
Ethan-Chiu <ethanchiu0608@gmail.com>
2024-09-03 11:30:11 +0800

    Fix type for helper functions

--- src/server/database/mapper/helper_function.ts
+++ src/server/database/mapper/helper_function.ts
@@ -1,23 +1,23 @@
 import { get_date_string } from "~/server/service/helper_function";
 
-export function convertDatePropertiesToISOString(obj: any): any {
+export function convertDatePropertiesToISOString<T extends object>(obj: T): T {
     for (const prop in obj) {
-        if (obj.hasOwnProperty(prop) && obj[prop] instanceof Date) {
+        if (Object.prototype.hasOwnProperty.call(obj, prop) && obj[prop] instanceof Date) {
             if (prop === "start_date" || prop === "end_date") {
-                obj[prop] = get_date_string(obj[prop]);
+                (obj[prop] as unknown) = get_date_string(obj[prop as keyof T]);
             } else {
-                obj[prop] = obj[prop].toISOString();
+                (obj[prop] as unknown) = (obj[prop as keyof T] as Date).toISOString();
             }
         }
     }
     return obj;
 }
 
-export function deleteProperties(obj: any, props: string[]): any {
+export function deleteProperties<T extends object>(obj: T, props: string[]): T {
     for (const prop of props) {
-        if (obj.hasOwnProperty(prop)) {
-            delete obj[prop];
-        }
+      if (Object.prototype.hasOwnProperty.call(obj, prop)) {
+        delete obj[prop as keyof T];
+      }
     }
     return obj;
 }
