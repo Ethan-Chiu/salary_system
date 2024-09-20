0ceca7afa609714a8f88a0292e1f6295f6e6369c
Ethan-Chiu <ethanchiu0608@gmail.com>
2024-09-20 15:54:41 +0800

    Fix employee payment promise all and invalidate.

--- src/server/service/employee_payment_service.ts
+++ src/server/service/employee_payment_service.ts
@@ -7,7 +7,10 @@ import { Op } from "sequelize";
 import { EHRService } from "./ehr_service";
 import { LevelRangeService } from "./level_range_service";
 import { LevelService } from "./level_service";
-import { createEmployeePaymentService, updateEmployeePaymentService } from "../api/types/employee_payment_type";
+import {
+	createEmployeePaymentService,
+	updateEmployeePaymentService,
+} from "../api/types/employee_payment_type";
 import { EmployeePaymentMapper } from "../database/mapper/employee_payment_mapper";
 import { EmployeeDataService } from "./employee_data_service";
 
@@ -30,23 +33,26 @@ export class EmployeePaymentService {
 	}: z.infer<typeof createEmployeePaymentService>): Promise<EmployeePayment> {
 		const current_date_string = get_date_string(new Date());
 		check_date(start_date, end_date, current_date_string);
-		const newData = await EmployeePayment.create({
-			emp_no: emp_no,
-			base_salary_enc: base_salary_enc,
-			food_allowance_enc: food_allowance_enc,
-			supervisor_allowance_enc,
-			occupational_allowance_enc,
-			subsidy_allowance_enc,
-			l_r_self_enc: l_r_self_enc,
-			l_i_enc: l_i_enc,
-			h_i_enc: h_i_enc,
-			l_r_enc: l_r_enc,
-			occupational_injury_enc: occupational_injury_enc,
-			start_date: start_date ?? current_date_string,
-			end_date: end_date,
-			create_by: "system",
-			update_by: "system",
-		}, { raw: true });
+		const newData = await EmployeePayment.create(
+			{
+				emp_no: emp_no,
+				base_salary_enc: base_salary_enc,
+				food_allowance_enc: food_allowance_enc,
+				supervisor_allowance_enc,
+				occupational_allowance_enc,
+				subsidy_allowance_enc,
+				l_r_self_enc: l_r_self_enc,
+				l_i_enc: l_i_enc,
+				h_i_enc: h_i_enc,
+				l_r_enc: l_r_enc,
+				occupational_injury_enc: occupational_injury_enc,
+				start_date: start_date ?? current_date_string,
+				end_date: end_date,
+				create_by: "system",
+				update_by: "system",
+			},
+			{ raw: true }
+		);
 		return newData;
 	}
 
@@ -59,7 +65,9 @@ export class EmployeePaymentService {
 		return employeePayment;
 	}
 
-	async getEmployeePaymentByEmpNo(emp_no: string): Promise<EmployeePayment | null> {
+	async getEmployeePaymentByEmpNo(
+		emp_no: string
+	): Promise<EmployeePayment | null> {
 		const employeePayment = await EmployeePayment.findOne({
 			where: {
 				emp_no: emp_no,
@@ -86,10 +94,8 @@ export class EmployeePaymentService {
 					],
 				},
 			},
-			order: [
-				["emp_no", "ASC"]
-			],
-			raw: true
+			order: [["emp_no", "ASC"]],
+			raw: true,
 		});
 		return employeePayment;
 	}
@@ -114,9 +120,7 @@ export class EmployeePaymentService {
 					],
 				},
 			},
-			order: [
-				["emp_no", "ASC"]
-			]
+			order: [["emp_no", "ASC"]],
 		});
 		return employeePayment;
 	}
@@ -146,10 +150,8 @@ export class EmployeePaymentService {
 	}
 	async getAllEmployeePayment(): Promise<EmployeePayment[]> {
 		const employeePayment = await EmployeePayment.findAll({
-			order: [
-				["emp_no", "ASC"]
-			],
-			raw: true
+			order: [["emp_no", "ASC"]],
+			raw: true,
 		});
 		return employeePayment;
 	}
@@ -203,10 +205,7 @@ export class EmployeePaymentService {
 				),
 				l_i_enc: select_value(l_i_enc, employeePayment.l_i_enc),
 				h_i_enc: select_value(h_i_enc, employeePayment.h_i_enc),
-				l_r_enc: select_value(
-					l_r_enc,
-					employeePayment.l_r_enc
-				),
+				l_r_enc: select_value(l_r_enc, employeePayment.l_r_enc),
 				occupational_injury_enc: select_value(
 					occupational_injury_enc,
 					employeePayment.occupational_injury_enc
@@ -237,24 +236,37 @@ export class EmployeePaymentService {
 	async autoCalculateEmployeePayment(
 		period_id: number,
 		emp_no_list: string[]
-	): Promise<void> {
+	): Promise<{ message: string }> {
 		const levelRangeService = container.resolve(LevelRangeService);
 		const levelService = container.resolve(LevelService);
 		const employeeDataService = container.resolve(EmployeeDataService);
 		const employeePaymentMapper = container.resolve(EmployeePaymentMapper);
-		emp_no_list.forEach(async (emp_no: string) => {
+
+		const levelRangeList = await levelRangeService.getAllLevelRange();
+
+		const promises = emp_no_list.map(async (emp_no: string) => {
 			const employeePayment = await this.getCurrentEmployeePaymentByEmpNo(
 				emp_no,
 				period_id
 			);
+
 			if (employeePayment == null) {
 				throw new BaseResponseError("Employee Payment does not exist");
 			}
-			const employeePaymentFE = await employeePaymentMapper.getEmployeePaymentFE(employeePayment.dataValues);
-			const levelRangeList = await levelRangeService.getAllLevelRange();
+
+			const employeePaymentFE =
+				await employeePaymentMapper.getEmployeePaymentFE(
+					employeePayment.dataValues
+				);
+
 			const salary =
-				employeePaymentFE.base_salary + (employeePaymentFE.food_allowance ?? 0) + (employeePaymentFE.supervisor_allowance ?? 0) + (employeePaymentFE.occupational_allowance ?? 0) + (employeePaymentFE.subsidy_allowance ?? 0);
-			let result = [];
+				employeePaymentFE.base_salary +
+				(employeePaymentFE.food_allowance ?? 0) +
+				(employeePaymentFE.supervisor_allowance ?? 0) +
+				(employeePaymentFE.occupational_allowance ?? 0) +
+				(employeePaymentFE.subsidy_allowance ?? 0);
+
+			const result = [];
 			for (const levelRange of levelRangeList) {
 				const level = await levelService.getLevelBySalary(
 					salary,
@@ -266,56 +278,47 @@ export class EmployeePaymentService {
 					level: level.level,
 				});
 			}
-			const employeeData = await employeeDataService.getEmployeeDataByEmpNo(emp_no);
+
+			const employeeData =
+				await employeeDataService.getEmployeeDataByEmpNo(emp_no);
 			if (employeeData == null) {
 				throw new BaseResponseError("Employee Data does not exist");
 			}
-			const updatedEmployeePayment = await employeePaymentMapper.getEmployeePayment({
-				...employeePaymentFE,
-				l_i: result.find((r) => r.type === "勞保")?.level ?? 0,
-				h_i: result.find((r) => r.type === "健保")?.level ?? 0,
-				l_r: employeeData.work_type != "外籍勞工" ? result.find((r) => r.type === "勞退")?.level ?? 0 : 0,
-				occupational_injury: result.find((r) => r.type === "職災")?.level ?? 0,
-			});
 
-			// const affectedCount = await EmployeePayment.update(
-			// 	{
-			// 		// emp_no: select_value(emp_no, employeePayment.emp_no),
-			// 		// base_salary_enc: 	employeePayment.base_salary_enc,
-			// 		// food_allowance_enc:	employeePayment.food_allowance_enc,
-			// 		// supervisor_allowance_enc:	employeePayment.supervisor_allowance_enc,
-			// 		// occupational_allowance_enc:	employeePayment.occupational_allowance_enc,
-			// 		// subsidy_allowance_enc: 		employeePayment.subsidy_allowance_enc,
-			// 		// l_r_self_enc: 				employeePayment.l_r_self_enc,
-			// 		l_i_enc: select_value(updatedEmployeePayment.l_i_enc, employeePayment.l_i_enc),
-			// 		h_i_enc: select_value(updatedEmployeePayment.h_i_enc, employeePayment.h_i_enc),
-			// 		l_r_enc: select_value(updatedEmployeePayment.l_r_enc, employeePayment.l_r_enc),
-			// 		// occupational_injury_enc: select_value(
-			// 		// 	updatedEmployeePayment.occupational_injury_enc,
-			// 		// 	employeePayment.occupational_injury_enc
-			// 		// ),
-			// 		// start_date: 			 employeePayment.start_date,
-			// 		// end_date: 				 employeePayment.end_date,
-			// 		update_by: "system",
-			// 	},
-			// 	{ where: { emp_no: emp_no } }
-			// );
+			const updatedEmployeePayment =
+				await employeePaymentMapper.getEmployeePayment({
+					...employeePaymentFE,
+					l_i: result.find((r) => r.type === "勞保")?.level ?? 0,
+					h_i: result.find((r) => r.type === "健保")?.level ?? 0,
+					l_r:
+						employeeData.work_type != "外籍勞工"
+							? result.find((r) => r.type === "勞退")?.level ?? 0
+							: 0,
+					occupational_injury:
+						result.find((r) => r.type === "職災")?.level ?? 0,
+				});
 
+			/* Update database */
 			const affectedCount = await EmployeePayment.update(
 				{
 					l_i_enc: updatedEmployeePayment.l_i_enc,
 					h_i_enc: updatedEmployeePayment.h_i_enc,
 					l_r_enc: updatedEmployeePayment.l_r_enc,
-					occupational_injury_enc: updatedEmployeePayment.occupational_injury_enc,
+					occupational_injury_enc:
+						updatedEmployeePayment.occupational_injury_enc,
 					update_by: "system",
 				},
 				{ where: { emp_no: emp_no } }
 			);
-			
+
 			if (affectedCount[0] == 0) {
 				throw new BaseResponseError("Update error");
 			}
 		});
+
+		await Promise.all(promises);
+
+		return { message: "success" };
 	}
 
 	async rescheduleEmployeePayment(): Promise<void> {
@@ -336,18 +339,17 @@ export class EmployeePaymentService {
 			const new_end_date_string = get_date_string(
 				new Date(start_date.setDate(start_date.getDate() - 1))
 			);
-			if (employeePaymentList[i]!.dataValues.emp_no ==
-				employeePaymentList[i + 1]!.dataValues.emp_no) {
-					if (
-						end_date_string != new_end_date_string
-					) {
-						await this.updateEmployeePayment({
-							id: employeePaymentList[i]!.dataValues.id,
-							end_date: new_end_date_string,
-						});
-					}
-			}
-			else {
+			if (
+				employeePaymentList[i]!.dataValues.emp_no ==
+				employeePaymentList[i + 1]!.dataValues.emp_no
+			) {
+				if (end_date_string != new_end_date_string) {
+					await this.updateEmployeePayment({
+						id: employeePaymentList[i]!.dataValues.id,
+						end_date: new_end_date_string,
+					});
+				}
+			} else {
 				await this.updateEmployeePayment({
 					id: employeePaymentList[i]!.dataValues.id,
 					end_date: null,
@@ -355,7 +357,8 @@ export class EmployeePaymentService {
 			}
 		}
 		await this.updateEmployeePayment({
-			id: employeePaymentList[employeePaymentList.length - 1]!.dataValues.id,
+			id: employeePaymentList[employeePaymentList.length - 1]!.dataValues
+				.id,
 			end_date: null,
 		});
 	}
