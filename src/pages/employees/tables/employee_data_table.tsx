a3f3c7aee426ff09baff0a5b1d5034ddee2fcbcf
Pony Wang <ponywang10@gmail.com>
2024-10-11 12:36:44 +0800

    fix merge

--- src/pages/employees/tables/employee_data_table.tsx
+++ src/pages/employees/tables/employee_data_table.tsx
@@ -3,8 +3,6 @@ import { DataTable } from "../components/data_table";
 import { api } from "~/utils/api";
 import { type I18nType } from "~/lib/utils/i18n_type";
 import { useTranslation } from "react-i18next";
-import { Button } from "~/components/ui/button";
-import { ArrowUpDown } from "lucide-react";
 
 const columns = (t: I18nType) =>
 	[
@@ -29,26 +27,7 @@ const columns = (t: I18nType) =>
 	].map((key) => {
 		return {
 			accessorKey: key,
-			header: ({ column }:any) => {
-				const { t } = useTranslation(["common"]);
-				return (
-					<div className="flex justify-center">
-						<div className="text-center font-medium">
-							<Button
-								variant="ghost"
-								onClick={() =>
-									column.toggleSorting(
-										column.getIsSorted() === "asc"
-									)
-								}
-							>
-								{t(`table.${key}`)}
-								<ArrowUpDown className="ml-2 h-4 w-4" />
-							</Button>
-						</div>
-					</div>
-				);
-			},
+			header: t(`table.${key}`),
 		};
 	});
 
--- src/pages/employees/tables/employee_trust_table.tsx
+++ src/pages/employees/tables/employee_trust_table.tsx
@@ -3,9 +3,6 @@ import { DataTableUpdate } from "../components/data_table_update";
 import { api } from "~/utils/api";
 import { type I18nType } from "~/lib/utils/i18n_type";
 import { useTranslation } from "react-i18next";
-import { Button } from "~/components/ui/button";
-import { ArrowUpDown } from "lucide-react";
-
 
 const columns = (t: I18nType) =>
 	[
@@ -23,26 +20,7 @@ const columns = (t: I18nType) =>
 	].map((key) => {
 		return {
 			accessorKey: key,
-			header: ({ column }:any) => {
-				const { t } = useTranslation(["common"]);
-				return (
-					<div className="flex justify-center">
-						<div className="text-center font-medium">
-							<Button
-								variant="ghost"
-								onClick={() =>
-									column.toggleSorting(
-										column.getIsSorted() === "asc"
-									)
-								}
-							>
-								{t(`table.${key}`)}
-								<ArrowUpDown className="ml-2 h-4 w-4" />
-							</Button>
-						</div>
-					</div>
-				);
-			},
+			header: t(`table.${key}`),
 		};
 	});
 
