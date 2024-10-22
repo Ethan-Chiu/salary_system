100aa702ee4b9c60b494551b8e06e2a52520e2b2
Ethan-Chiu <ethanchiu0608@gmail.com>
2024-10-22 16:48:55 +0800

    Expanse model class.

--- src/server/database/entity/UMEDIA/expense.ts
+++ src/server/database/entity/UMEDIA/expense.ts
@@ -1,37 +1,48 @@
 import { z } from "zod";
 
-/* const dbEmp = z.object({ */
-/**/
-/* }); */
+const dbExpense = z.object({
+  PERIOD_ID: z.number(),
+  KIND: z.number(),
+  EMP_NO: z.string(),
+  EMP_NAME: z.string(),
+  ID: z.number(),
+  AMOUNT: z.number(),
+  REMARK: z.string(),
+  CREATE_BY: z.string(),
+  CREATE_DATE: z.date(),
+  UPDATE_BY: z.string(),
+  UPDATE_DATE: z.date(),
+  PAY_DELAY: z.number(),
+});
 
 export class Expense {
 	// id can be undefined during creation when using `autoIncrement`
-	period_id?: number;
-	kind?: number;
-	emp_no?: string;
-	emp_name?: string;
-	id?: number;
-	amount?: number;
-	remark?: string;
-	create_by?: string;
-	create_date?: Date;
-	update_by?: string;
-	update_date?: Date;
-	pay_delay?: number;
+	period_id: number;
+	kind: number;
+	emp_no: string;
+	emp_name: string;
+	id: number;
+	amount: number;
+	remark: string;
+	create_by: string;
+	create_date: Date;
+	update_by: string;
+	update_date: Date;
+	pay_delay: number;
 
 	constructor(
-		period_id?: number,
-		kind?: number,
-		emp_no?: string,
-		emp_name?: string,
-		id?: number,
-		amount?: number,
-		remark?: string,
-		create_by?: string,
-		create_date?: Date,
-		update_by?: string,
-		update_date?: Date,
-		pay_delay?: number
+		period_id: number,
+		kind: number,
+		emp_no: string,
+		emp_name: string,
+		id: number,
+		amount: number,
+		remark: string,
+		create_by: string,
+		create_date: Date,
+		update_by: string,
+		update_date: Date,
+		pay_delay: number
 	) {
 		this.period_id = period_id;
 		this.kind = kind;
@@ -46,35 +57,29 @@ export class Expense {
 		this.update_date = update_date;
 		this.pay_delay = pay_delay;
 	}
-	static fromDB(data: any): Expense {
-		const {
-			PERIOD_ID,
-			KIND,
-			EMP_NO,
-			EMP_NAME,
-			ID,
-			AMOUNT,
-			REMARK,
-			CREATE_BY,
-			CREATE_DATE,
-			UPDATE_BY,
-			UPDATE_DATE,
-			PAY_DELAY,
-		} = data;
+
+	static fromDB(db_data: any): Expense {
+		const result = dbExpense.safeParse(db_data);
+
+		if (!result.success) {
+			throw new Error(result.error.message);
+		}
+
+		const data = result.data;
 
 		return new Expense(
-			PERIOD_ID,
-			KIND,
-			EMP_NO,
-			EMP_NAME,
-			ID,
-			Number(AMOUNT),
-			REMARK,
-			CREATE_BY,
-			CREATE_DATE,
-			UPDATE_BY,
-			UPDATE_DATE,
-			PAY_DELAY
+			data.PERIOD_ID,
+			data.KIND,
+			data.EMP_NO,
+			data.EMP_NAME,
+			data.ID,
+			Number(data.AMOUNT),
+			data.REMARK,
+			data.CREATE_BY,
+			data.CREATE_DATE,
+			data.UPDATE_BY,
+			data.UPDATE_DATE,
+			data.PAY_DELAY
 		);
 	}
 }
