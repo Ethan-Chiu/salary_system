a22b6e5d28a22222f95256b8658d5f80ba1dd0f6
Ethan-Chiu <ethanchiu0608@gmail.com>
2024-10-25 13:54:59 +0800

    Payset data model

--- src/server/database/entity/UMEDIA/payset.ts
+++ src/server/database/entity/UMEDIA/payset.ts
@@ -1,32 +1,51 @@
+import { z } from "zod";
+
+const dbPayset = z.object({
+	PERIOD_ID: z.number(),
+	PERIOD_NAME: z.string(),
+	STATUS: z.string(),
+	NID: z.number(),
+	EMPLOYEE_NO: z.string(),
+	NAME: z.string(),
+	COUNTRY_ID: z.number(),
+	PAY_TYPE: z.number(),
+	PAY_SET: z.number(),
+	PAY_DELAY: z.number(),
+	WORK_DAY: z.number(),
+	LI_DAY: z.number(),
+	DELAY_COUNT: z.number(),
+});
+
 export class Payset {
 	// id can be undefined during creation when using `autoIncrement`
-	declare period_id?: number;
-	declare period_name?: string;
-	declare status?: string;
-	declare nid?: number;
-	declare emp_no?: string;
-	declare name?: string;
-	declare country_id?: number;
-	declare pay_type?: number;
-	declare pay_set?: number;
-	declare pay_delay?: number;
-	declare work_day?: number;
-	declare li_day?: number;
-	declare delay_count?: number;
+	period_id: number;
+	period_name: string;
+	status: string;
+	nid: number;
+	emp_no: string;
+	name: string;
+	country_id: number;
+	pay_type: number;
+	pay_set: number;
+	pay_delay: number;
+	work_day: number;
+	li_day: number;
+	delay_count: number;
+
 	constructor(
-		period_id?: number,
-		period_name?: string,
-		status?: string,
-		nid?: number,
-		emp_no?: string,
-		name?: string,
-		country_id?: number,
-		pay_type?: number,
-		pay_set?: number,
-		pay_delay?: number,
-		work_day?: number,
-		li_day?: number,
-		delay_count?: number
+		period_id: number,
+		period_name: string,
+		status: string,
+		nid: number,
+		emp_no: string,
+		name: string,
+		country_id: number,
+		pay_type: number,
+		pay_set: number,
+		pay_delay: number,
+		work_day: number,
+		li_day: number,
+		delay_count: number
 	) {
 		this.period_id = period_id;
 		this.period_name = period_name;
@@ -42,37 +61,30 @@ export class Payset {
 		this.li_day = li_day;
 		this.delay_count = delay_count;
 	}
-	static fromDB(data: any): Payset {
-		const {
-			PERIOD_ID,
-			PERIOD_NAME,
-			STATUS,
-			NID,
-			EMPLOYEE_NO,
-			NAME,
-			COUNTRY_ID,
-			PAY_TYPE,
-			PAY_SET,
-			PAY_DELAY,
-			WORK_DAY,
-			LI_DAY,
-			DELAY_COUNT,
-		} = data;
+
+	static fromDB(db_data: any): Payset {
+		const result = dbPayset.safeParse(db_data);
+
+		if (!result.success) {
+			throw new Error(result.error.message);
+		}
+
+		const data = result.data;
 
 		return new Payset(
-			PERIOD_ID,
-			PERIOD_NAME,
-			STATUS,
-			NID,
-			EMPLOYEE_NO,
-			NAME,
-			COUNTRY_ID,
-			PAY_TYPE,
-			PAY_SET,
-			PAY_DELAY,
-			WORK_DAY,
-			LI_DAY,
-			DELAY_COUNT
+			data.PERIOD_ID,
+			data.PERIOD_NAME,
+			data.STATUS,
+			data.NID,
+			data.EMPLOYEE_NO,
+			data.NAME,
+			data.COUNTRY_ID,
+			data.PAY_TYPE,
+			data.PAY_SET,
+			data.PAY_DELAY,
+			data.WORK_DAY,
+			data.LI_DAY,
+			data.DELAY_COUNT
 		);
 	}
 }
