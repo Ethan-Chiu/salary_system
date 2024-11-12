5dadef642f2d947508450b8137e95edcc16e25ec
Ethan-Chiu <ethanchiu0608@gmail.com>
2024-11-12 15:05:50 +0800

    Fix data enc dec.

--- src/server/database/mapper/mapper_utils.ts
+++ src/server/database/mapper/mapper_utils.ts
@@ -2,12 +2,15 @@ import { z } from "zod";
 import { dateToString, dateToStringNullable } from "~/server/api/types/z_utils";
 
 export const encDate = z.object({
-  start_date: z.string().pipe(z.coerce.date()),
-  end_date: z.string().nullable().pipe(z.coerce.date()),
-})
+	start_date: z.string().pipe(z.coerce.date()),
+	end_date: z
+		.string()
+		.nullable()
+		.transform((value) => (value === null ? null : new Date(value)))
+		.pipe(z.date().nullable()),
+});
 
 export const decDate = z.object({
 	start_date: dateToString,
-	end_date: dateToStringNullable, 
-})
-
+	end_date: dateToStringNullable,
+});
