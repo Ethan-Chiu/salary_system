6997575579cdd686bb7ea998ada944447bd8ad9a
Howard-149 <b09901149@g.ntu.edu.tw>
2024-11-29 16:07:03 +0800

    add updateMultipleBonus

--- src/server/service/employee_bonus_service.ts
+++ src/server/service/employee_bonus_service.ts
@@ -380,6 +380,37 @@ export class EmployeeBonusService {
 			),
 		});
 	}
+	async updateMultipleBonusByEmpNoList(
+		emp_no_list: string[],
+		period_id: number,
+		bonus_type: BonusTypeEnumType,
+		multiplier: number,
+		fixed_amount: number
+	): Promise<void> {
+		await Promise.all(
+			emp_no_list.map(async (emp_no) => {
+				const emp_bonus = await this.getEmployeeBonusByEmpNoByType(
+					period_id,
+					bonus_type,
+					emp_no
+				)
+				if (!emp_bonus) {
+					throw new BaseResponseError(
+						"Employee bonus does not exist"
+					)
+				}
+				await this.updateEmployeeBonus({
+					id: emp_bonus.id,
+					multiplier_enc: CryptoHelper.encrypt(
+						multiplier.toString()
+					),
+					fixed_amount_enc: CryptoHelper.encrypt(
+						fixed_amount.toString()
+					)
+				});
+			})
+		);
+	}
 	async updateFromExcel(
 		force: boolean,
 		{
