64daef4b48b9ef455253d9f83c98af37c420008e
Ethan-Chiu <ethanchiu0608@gmail.com>
2024-11-29 12:00:20 +0800

    Level encode decode.

--- src/server/database/entity/SALARY/level.ts
+++ src/server/database/entity/SALARY/level.ts
@@ -4,8 +4,45 @@ import {
     type InferAttributes,
     type InferCreationAttributes,
     type CreationOptional,
-    Sequelize,
+    type Sequelize,
 } from "sequelize";
+import { z } from "zod";
+import { dateF, dateStringF, systemF, systemKeys } from "../../mapper/mapper_utils";
+import { dateToString, dateToStringNullable, stringToDate, stringToDateNullable } from "~/server/api/types/z_utils";
+
+const dbLevel = z.object({
+  level: z.number(),
+	create_by: z.string(),
+	update_by: z.string(),
+	disabled: z.coerce.boolean(),
+});
+
+const decFields = z.object({
+	id: z.number(),
+});
+
+const encF = dbLevel.merge(dateStringF);
+const decF = dbLevel.merge(decFields).merge(dateF);
+export type LevelDecType = z.input<typeof decF>;
+
+export const decLevel = encF
+	.merge(systemF)
+	.transform((v) => ({
+		...v,
+		id: v.id,
+		start_date: stringToDate.parse(v.start_date),
+		end_date: stringToDateNullable.parse(v.end_date),
+	}))
+	.pipe(decF);
+
+export const encLevel = decF
+	.omit(systemKeys)
+	.transform((v) => ({
+		...v,
+		start_date: dateToString.parse(v.start_date),
+		end_date: dateToStringNullable.parse(v.end_date),
+	}))
+	.pipe(encF);
 
 export class Level extends Model<
     InferAttributes<Level>,
