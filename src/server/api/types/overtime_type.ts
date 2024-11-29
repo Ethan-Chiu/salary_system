5aeff1fe2d7f1a42e49c78aac8d966be83150450
Howard-149 <b09901149@g.ntu.edu.tw>
2024-11-29 13:17:00 +0800

    overtime and holiday mapper

---
+++ src/server/api/types/overtime_type.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+
+export const OvertimeFE=z.object({
+    department: z.string(),
+    emp_no: z.string(),
+    emp_name: z.string(),
+    position: z.number(),
+    work_day: z.number(),
+    // period_id: number;
+	// pay: number;
+	// type_id: number;
+	// days_radio: string;
+	// type_name: string;
+	// pay_period: number | null;
+	// period_name: string | null;
+	// pay_delay: number | null;
+	hours_1: z.number(),
+	hours_134: z.number(),
+	hours_167: z.number(),
+	hours_267: z.number(),
+	hours_2: z.number(),
+	hours_134_TAX: z.number(),
+	hours_167_TAX: z.number(),
+	hours_267_TAX: z.number(),
+	hours_2_TAX: z.number()
+})
+
+export type OvertimeFEType = z.infer<typeof OvertimeFE>
