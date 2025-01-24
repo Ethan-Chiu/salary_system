f497015953645f867bebaf80a0628f316fcd14e1
Ethan-Chiu <ethanchiu0608@gmail.com>
2025-01-24 14:33:01 +0800

    Validate date dialog with form.

--- src/pages/employees/components/function_sheet/date_dialog.tsx
+++ src/pages/employees/components/function_sheet/date_dialog.tsx
@@ -8,7 +8,10 @@ import {
 } from "~/components/ui/dialog";
 import { DatePicker } from "~/components/ui/date-picker";
 import { Button } from "~/components/ui/button";
-import { useState } from "react";
+import { z } from "zod";
+import { Form, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FormField } from "~/components/ui/form";
 
 interface DateDialogProps {
 	open: boolean;
@@ -16,9 +19,26 @@ interface DateDialogProps {
 	onSubmit: (date: Date) => void;
 }
 
-// TODO: validate date is valid
-export function DateDialog({ open, setOpen, onSubmit }: DateDialogProps) {
-	const [date, setDate] = useState<Date>(new Date());
+const dateDialogSchema = z.object({
+	date: z.date(),
+});
+
+// TODO: validate date is valid (disable invalid value)
+export function DateDialog({
+	open,
+	setOpen,
+	onSubmit: submit,
+}: DateDialogProps) {
+	const form = useForm<z.infer<typeof dateDialogSchema>>({
+		resolver: zodResolver(dateDialogSchema),
+		defaultValues: {
+			date: new Date(),
+		},
+	});
+
+	const onSubmit = (data: z.infer<typeof dateDialogSchema>) => {
+    submit(data.date);
+  };
 
 	return (
 		<Dialog open={open} onOpenChange={setOpen}>
@@ -31,16 +51,26 @@ export function DateDialog({ open, setOpen, onSubmit }: DateDialogProps) {
 						servers.
 					</DialogDescription>
 				</DialogHeader>
-				<DatePicker
-					date={date}
-					setDate={(d) => {
-						if (d) {
-							setDate(d);
+				<Form {...form}>
+					<form
+						onSubmit={(event) =>
+							void form.handleSubmit(onSubmit)(event)
 						}
-					}}
-				/>
+					>
+						<FormField
+							control={form.control}
+							name="date"
+							render={({ field }) => (
+								<DatePicker
+									date={field.value}
+									setDate={field.onChange}
+								/>
+							)}
+						/>
+					</form>
+				</Form>
 				<DialogFooter>
-					<Button onClick={() => onSubmit(date)}>Confirm</Button>
+					<Button type="submit">Confirm</Button>
 				</DialogFooter>
 			</DialogContent>
 		</Dialog>
